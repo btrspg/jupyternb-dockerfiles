109ae44a04132f1173ffe9f0acd2650fa27cee13
logan <yuelong.chen.btr@gmail.com>
2021-03-31 22:06:25 +0800

    :bug: add the R dependencies

--- single_cell/install_Rpackages.R
+++ single_cell/install_Rpackages.R
@@ -2,12 +2,15 @@
 
 options(install.packages.compile.from.source = "always")
 chooseCRANmirror(ind=17)
-packages <- c('devtools','IRkernel')
+packages <- c('devtools','IRkernel','BiocManager')
 install.packages(packages)
 
+BiocManager::install("Biobase")
+BiocManager::install("BiocGenerics")
 
 
 
+devtools::install_github("jokergoo/ComplexHeatmap",upgrade_dependencies=FALSE)
 remotes::install_github("satijalab/seurat",upgrade_dependencies=FALSE)
 remotes::install_github("mojaveazure/seurat-disk",upgrade_dependencies=FALSE)
 devtools::install_github("sqjin/CellChat",upgrade_dependencies=FALSE)
