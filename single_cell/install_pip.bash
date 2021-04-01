527735cb86a7ebb517e725af160dca2db90d7af2
Logan <yuelong.chen.btr@gmail.com>
2021-04-01 09:44:30 +0800

    :bug: feat: add whole environment

--- single_cell/install_pip.bash
+++ single_cell/install_pip.bash
@@ -4,7 +4,7 @@ python3 -m pip --no-cache-dir install pip --upgrade
 python3 -m pip --no-cache-dir install setuptools --upgrade
 python3 -m pip --no-cache-dir install wheel --upgrade
 pip install --no-cache-dir -r requirements.txt
-
+jupyter kernelspec --version
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 rm -rf /root/.cache
