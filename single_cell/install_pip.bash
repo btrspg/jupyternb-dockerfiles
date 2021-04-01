0d0426c1c1cc20dcab7fd003d06800373544ddf9
Logan <yuelong.chen.btr@gmail.com>
2021-04-01 15:28:13 +0800

    :white_check_mark: specify the kernel name

--- single_cell/install_pip.bash
+++ single_cell/install_pip.bash
@@ -4,6 +4,7 @@ python3 -m pip --no-cache-dir install pip --upgrade
 python3 -m pip --no-cache-dir install setuptools --upgrade
 python3 -m pip --no-cache-dir install wheel --upgrade
 pip install --no-cache-dir -r /opt/requirements.txt
+python3 -m ipykernel install --name python384SC --display-name "Python38 4SC"
 jupyter kernelspec --version
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
