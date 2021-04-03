8cdda6bef5d174d24c7c44801bcb9c3e77a012b3
Logan <yuelong.chen.btr@gmail.com>
2021-04-03 14:32:12 +0800

    :white_check_mark: pip environment update

--- nanopore/install_pip.bash
+++ nanopore/install_pip.bash
@@ -4,6 +4,7 @@ python3 -m pip --no-cache-dir install pip --upgrade
 python3 -m pip --no-cache-dir install setuptools --upgrade
 python3 -m pip --no-cache-dir install wheel --upgrade
 pip3 install --no-cache-dir -r /opt/requirements.txt
+pip3 install ont-tombo
 python3 -m ipykernel install --name python384NP --display-name "Python38 4NP"
 jupyter kernelspec --version
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
