c6cd77daa1654ff3e961ca14793d1ddfe1f85305
Logan <yuelong.chen.btr@gmail.com>
2021-05-11 14:38:30 +0800

    :heavy_plus_sign: add packages in dev

--- general_dev/install_Rpackages.R
+++ general_dev/install_Rpackages.R
@@ -2,11 +2,17 @@
 
 options(install.packages.compile.from.source = "always")
 chooseCRANmirror(ind=0)
-packages <- c('devtools','IRkernel','BiocManager')
+packages <- c('devtools','IRkernel','BiocManager','tidyverse')
 install.packages(packages)
 
 BiocManager::install("Biobase")
 BiocManager::install("BiocGenerics")
+BiocManager::install("sva")
+BiocManager::install("sva")
+BiocManager::install("DESeq2")
+BiocManager::install("edgeR")
+BiocManager::install("EBSeq")
+devtools::install_github("jokergoo/ComplexHeatmap",upgrade_dependencies=FALSE)
 IRkernel::installspec(name = 'ir404GD', displayname = 'R_4GD',user = FALSE)
 
 
