4ed51ac7d593fe3fe3cb709dfd31763c5d31cc05
Yuelong Logan Chen <yuelong.chen.btr@gmail.com>
2021-07-14 12:41:00 +0800

    ⬆️: install automatic annotation

--- single_cell/install_Rpackages.R
+++ single_cell/install_Rpackages.R
@@ -2,15 +2,21 @@
 
 options(install.packages.compile.from.source = "always")
 chooseCRANmirror(ind=0)
-packages <- c('devtools','IRkernel','BiocManager')
+packages <- c('devtools','IRkernel','BiocManager','rliger','SCINA')
 install.packages(packages)
 
 BiocManager::install("Biobase")
 BiocManager::install("BiocGenerics")
+BiocManager::install("scmap")
+BiocManager::install("scran")
 IRkernel::installspec(name = 'ir404SC', displayname = 'R_4SC',user = FALSE)
 
-
+devtools::install_github("pcahan1/singleCellNet")
+devtools::install_github(repo = "hhoeflin/hdf5r")
+devtools::install_github(repo = "mojaveazure/loomR", ref = "develop")
+devtools::install_github('dviraran/SingleR')
 devtools::install_github("jokergoo/ComplexHeatmap",upgrade_dependencies=FALSE)
+devtools::install_github('welch-lab/liger')
 remotes::install_github("satijalab/seurat",upgrade_dependencies=FALSE)
 remotes::install_github("mojaveazure/seurat-disk",upgrade_dependencies=FALSE)
 devtools::install_github("sqjin/CellChat",upgrade_dependencies=FALSE)
